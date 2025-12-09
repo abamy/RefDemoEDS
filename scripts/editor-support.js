c431300a2a1f49a9ef7d3927856850b34d8e4fba
bamy <arthur.bamy@ik.me>
2025-12-09 16:19:46 +0100

    Update editor-support.js

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -111,3 +111,35 @@ async function attachEventListners(main) {
 }
 
 attachEventListners(document.querySelector('main'));
+
+const url = new URL(window.location.href).pathname;
+const meta = document.createElement('meta');
+meta.name = 'urn:adobe:aue:config:disable';
+meta.content = 'duplicate,copy';
+document.getElementsByTagName('head')[0].appendChild(meta);
+
+const metaPreview = document.createElement('meta');
+metaPreview.name = 'urn:adobe:aue:config:preview';
+
+const siteName = url.match(/\/content\/([^\/]+)/)[1];
+
+const path = url
+  .replace(/\/content\/[^\/]+/, '')
+  .replace('/templates/', '/pages/')
+  .replace('.html', '');
+
+metaPreview.content = `http://main--${siteName}--abamy.aem.page${path}`;
+document.head.appendChild(metaPreview);
+
+if (!url.includes('/master/')) {
+  document.querySelectorAll('[data-aue-type]').forEach((el) => {
+    if (
+      el.tagName.toLowerCase() === 'body'
+      || el.getAttribute('data-aue-type') === 'container'
+      || el.getAttribute('data-aue-type') === 'column'
+    ) {
+      el.removeAttribute('data-aue-resource');
+      el.removeAttribute('data-aue-type');
+    }
+  });
+}
